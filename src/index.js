c1df00cd25b5680ddae18738848d704c3da0653e
lucasramallo <lucasdebrito.jd@gmail.com>
2023-01-20 19:23:14 -0300

    Atualizando

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-const db = require('./database/db')
-const App = require('./App/App')
+const db = require('./database/db');
+const App = require('./App/App');
 
-let UserInDataBase = db.username
-let passwordInDataBase = db.password 
-let emailInDataBase = db.email
+let UserInDataBase = db.username;
+let passwordInDataBase = db.password;
+let emailInDataBase = db.email;
 
 let TimeToAuth = 3000;
 
 const questions = [
     "User",
     "Password"
-]
+];
 
-const inputResponse = []
+const inputResponse = [];
 
 const input = (index = 0) => {
-    process.stdout.write(`${questions[index]} |> `)
+    process.stdout.write(`${questions[index]} |> `);
 }
-input()
+input();
 
 process.stdin.on("data", data =>{
-    inputResponse.push(data.toString().trim())
+    inputResponse.push(data.toString().trim());
     if(inputResponse.length < questions.length){
         input(inputResponse.length)
     } else if((inputResponse[0] == UserInDataBase || inputResponse[0] == emailInDataBase) && inputResponse[1] == passwordInDataBase){
-        setTimeout(() => {App()}, TimeToAuth)
-        console.log("\n⏱️ Authenticating...")
-        setTimeout(() => {process.exit()}, TimeToAuth)
+        setTimeout(() => {App()}, TimeToAuth);
+        console.log("\n⏱️ Authenticating...");
+        setTimeout(() => {process.exit()}, TimeToAuth);
     } else{
-        console.log("\n❌Incorrect Username/Email or Password. Please Try Again.")
-        process.exit()
+        console.log("\n❌Incorrect Username/Email or Password. Please Try Again.");
+        process.exit();
     
     }
-})
+});
